80a83fa0cc99ea5d5f76c231e67ceb8940ee8bcd
Padraic Corcoran <padraiccorcoran1@gmail.com>
2016-03-16 17:09:26 +0000

    Changed how to acces bcftools

--- scripts/samtools_snp_calling.sh
+++ scripts/samtools_snp_calling.sh
@@ -13,5 +13,7 @@ ls data/bam_files/*.bam > bams.list # create input list of bam files for samtool
 
 bcftools_1_3=/usr/local/extras/Genomics/apps/bcftools/1.3/bin/bcftools
 
-samtools mpileup -q 20 -Q 10 -ugf  ${REF_FILE} -b bams.list | ${bcftools_1_3} call -vmO z -f GQ -o ${VCF_OUT}/samtools.chrLGE22.raw.snps.indels.vcf.gz
+export PATH=/usr/local/extras/Genomics/apps/bcftools/1.3/bin/:$PATH
+
+samtools mpileup -q 20 -Q 10 -ugf  ${REF_FILE} -b bams.list | bcftools call -vmO z -f GQ -o ${VCF_OUT}/samtools.chrLGE22.raw.snps.indels.vcf.gz
 
